49524b7e331ce1a553f13da528da6ad12f4e1cb7
Matt <matthewglenverhey@gmail.com>
2021-08-16 19:04:58 -0500

    Initial commit

---
+++ web/scripts/main.js
@@ -0,0 +1,25 @@
+
+
+var logo = document.getElementById("logo-icon");
+var profileIcon = document.getElementById("profile-icon");
+
+document.getElementById("logo-icon").addEventListener("click", function () {
+    var icon = document.getElementById("left-side-menu");
+    if (icon.classList.contains("side-menu--hidden")) {
+        icon.classList.remove("side-menu--hidden");
+    }
+    else {
+        icon.classList.add("side-menu--hidden");
+    }
+})
+
+document.getElementById("profile-icon").addEventListener("click", function () {
+    var icon = document.getElementById("right-side-menu");
+    if (icon.classList.contains("side-menu--hidden")) {
+        icon.classList.remove("side-menu--hidden");
+    }
+    else {
+        icon.classList.add("side-menu--hidden");
+    }
+})
+
